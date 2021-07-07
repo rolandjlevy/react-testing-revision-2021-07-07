5edd7a2c690eab2502bc35ac2474336dcf59e78b
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-07-07 22:52:47 +0000

    updated tests

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button>clicker</button>
         <p>
           Edit <code>src/App.js</code>!
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { render, waitFor, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
 // import axios from 'axios';
 import App from './App';
-
-const Button = ({ onClick, children }) => (
-  <button 
-    onClick={onClick}
-  >
-    {children}
-  </button>
-)
+import Button from './Button';
 
 describe('Should render components, test existence and data', () => {
+  
   test('renders learn react link', () => {
     render(<App />);
     screen.getByRole('link', { name: 'Learn React'});
+    const btn = screen.getByRole('button', { name: 'clicker'});
+    userEvent.click(btn);
   });
 
   test('calls onClick prop when clicked', () => {
